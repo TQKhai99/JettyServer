4646d32ac1d7e0d978ccaaa2125ca9ac99f29081
TQKhai99 <quangkhai3499@gmail.com>
2021-08-09 17:02:22 +0700

    update HTTP server calls Thrift Service

--- src/main/java/Jetty.java
+++ src/main/java/Jetty.java
@@ -1,17 +1,18 @@
+import API.Like;
+import API.Song;
+import API.Stream;
 import org.eclipse.jetty.server.Server;
-
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-
+import org.eclipse.jetty.servlet.ServletHandler;
 
 public class Jetty {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         try{
-            ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+            ServletHandler context = new ServletHandler();
+            context.addServletWithMapping(Stream.class,"/song/stream");
+            context.addServletWithMapping(Like.class,"/song/like");
+            context.addServletWithMapping(Song.class,"/song");
             server.setHandler(context);
-            context.addServlet(new ServletHolder(new Api()),"/api");
-
             server.start();
             System.out.println("server started successful");
         }
